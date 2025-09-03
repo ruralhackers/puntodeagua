8a0c6ed08a96c5fc5e547084ba8df8d84626cb6d
Milky Raven <heartonllamas@gmail.com>
2025-09-03 19:08:44 +0200

    refactor: update water meter card with shadcn components

--- apps/webapp/src/features/water-meter/delivery/WaterMeter.page.tsx
+++ apps/webapp/src/features/water-meter/delivery/WaterMeter.page.tsx
@@ -3,7 +3,13 @@ import WaterMeterCard from "./components/WaterMeterCard";
 export default function WaterMeterPage() {
     return (
         <div>
-            <p>Listado</p>
+            <div>
+                <p>Contadores</p>
+                <p>Gestión de contadores y control de consumos</p>
+            </div>
+            <div>
+                <p>Barra de buscador por nombre</p>
+            </div>
             <WaterMeterCard />
         </div>
     )

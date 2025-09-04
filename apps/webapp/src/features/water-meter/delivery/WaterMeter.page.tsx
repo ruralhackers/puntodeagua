7e922448dd464138744bdd30a55315f3addd1de5
Milky Raven <heartonllamas@gmail.com>
2025-09-04 11:17:39 +0200

    chore: call water meter reading api

--- apps/webapp/src/features/water-meter/delivery/WaterMeter.page.tsx
+++ apps/webapp/src/features/water-meter/delivery/WaterMeter.page.tsx
@@ -1,32 +1,42 @@
-"use client"
+'use client'
 
-import { useEffect, useState } from "react";
-import WaterMeterCard from "./components/WaterMeterCard";
-
+import { useEffect, useState } from 'react'
+import WaterMeterCard from './components/WaterMeterCard'
 
 export default function WaterMeterPage() {
-    const [meters, setMeters] = useState([])
+  const [meters, setMeters] = useState([])
 
-    useEffect(() => {
-        //  llamar api aquí
-    }, [])
+  useEffect(() => {
+    const fetchMeters = async () => {
+      try {
+        const res = await fetch('http://localhost:4000/api/water-meters')
+        if (!res.ok) {
+          throw new Error()
+        }
+        const data = await res.json()
+        console.log(data)
+        setMeters(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMeters()
+  }, [])
 
-
-    return (
-        <div>
-            <div>
-                <h1>Contadores</h1>
-                <p>Gestión de contadores y control de consumos</p>
-            </div>
-            <div>
-                {/* <Input type="search" placeholder="Buscar por nombre..." /> */}
-                {/* el botón Filtrar debe abrir un modal */}
-                {/* <Button>Filtrar</Button> */}
-            </div>
-            {meters.map(() => (
-                <WaterMeterCard />
-            ))}
-            <WaterMeterCard />
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h1>Contadores</h1>
+        <p>Gestión de contadores y control de consumos</p>
+      </div>
+      <div>
+        {/* <Input type="search" placeholder="Buscar por nombre..." /> */}
+        {/* el botón Filtrar debe abrir un modal */}
+        {/* <Button>Filtrar</Button> */}
+      </div>
+      {meters.map((meter) => (
+        <WaterMeterCard key={meter.id} meter={meter} />
+      ))}
+    </div>
+  )
 }
--- apps/webapp/src/features/water-meter/delivery/components/WaterMeterCard.tsx
+++ apps/webapp/src/features/water-meter/delivery/components/WaterMeterCard.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
 import Link from 'next/link'
+import React from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 
-export default function WaterMeterCard() {
-    return (
-        // pasarle al link el id real del contador
-        <Link href="/contadores/id">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Nombre y apellidos</CardTitle>
-                    <div className="text-red-600 text-sm">Consumo anómalo (aviso que solo aparece si se sobrepasa el consumo)</div>
-                </CardHeader>
-                <CardContent>
-                    <p>Último consumo • fecha última lectura</p>
-                </CardContent>
-                <CardFooter>
-                    <p >Zona</p>
-                </CardFooter>
-            </Card>
-        </Link>
-    )
+export default function WaterMeterCard({ meter }: any) {
+  console.log('meter desde el card', meter)
+  return (
+    // pasarle al link el id real del contador
+    <Link href="/contadores/id">
+      <Card>
+        <CardHeader>
+          <CardTitle>{meter.name}</CardTitle>
+          <div className="text-red-600 text-sm">
+            Consumo anómalo (aviso que solo aparece si se sobrepasa el consumo)
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>Último consumo • fecha última lectura</p>
+        </CardContent>
+        <CardFooter>
+          <p>Zona</p>
+        </CardFooter>
+      </Card>
+    </Link>
+  )
 }

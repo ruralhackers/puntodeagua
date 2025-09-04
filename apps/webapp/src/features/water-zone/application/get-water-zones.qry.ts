8c8ccca8a0f4affdc539a02c1792e5c68f2afcfd
Orlando Martinez <roly@elevenyellow.com>
2025-09-04 12:42:05 +0200

    feat(water-zone): add water zone functionality with repository and DTO

---
+++ apps/webapp/src/features/water-zone/application/get-water-zones.qry.ts
@@ -0,0 +1,12 @@
+import type { Query } from 'core'
+import type { WaterZone, WaterZoneRepository } from 'features'
+
+export class GetWaterZonesQry implements Query<WaterZone[]> {
+  static readonly ID = 'GetWaterZonesQry'
+
+  constructor(private readonly repo: WaterZoneRepository) {}
+
+  async handle(): Promise<WaterZone[]> {
+    return await this.repo.findAll()
+  }
+}

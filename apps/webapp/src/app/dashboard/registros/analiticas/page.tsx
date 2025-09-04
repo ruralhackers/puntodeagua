b5b92122a0dff7715fed51c157a8baff58357399
Orlando Martinez <roly@elevenyellow.com>
2025-09-04 17:49:10 +0200

    feat(analysis): add analysis page with water zones integration

--- apps/webapp/src/app/dashboard/registros/analiticas/page.tsx
+++ apps/webapp/src/app/dashboard/registros/analiticas/page.tsx
@@ -1,9 +1,9 @@
 import { UseCaseService } from 'core'
 import type { NextPage } from 'next'
 import { GetWaterZonesQry } from '@/src/features/water-zone/application/get-water-zones.qry'
-import { webAppContainer } from '../../core/di/webapp.container'
-import { GetAnalysesQry } from '../../features/analysis/application/get-analyses.qry'
-import { AnalysisPage } from '../../features/analysis/delivery/analyses.page'
+import { webAppContainer } from '../../../../core/di/webapp.container'
+import { GetAnalysesQry } from '../../../../features/analysis/application/get-analyses.qry'
+import { AnalysisPage } from '../../../../features/analysis/delivery/analyses.page'
 
 const Page: NextPage = async () => {
   const analysis = await webAppContainer

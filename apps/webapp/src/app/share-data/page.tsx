30a411dc70128e120df7a1414e5ca8da680fba94
Milky Raven <heartonllamas@gmail.com>
2025-09-05 12:04:10 +0200

    refactor: create share data + refactor maintenance and analysis pages

---
+++ apps/webapp/src/app/share-data/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import ShareDataPage from '@/src/features/share-data/delivery/ShareData.page'
+
+const Page: NextPage = async () => {
+  return <ShareDataPage />
+}
+
+export default Page

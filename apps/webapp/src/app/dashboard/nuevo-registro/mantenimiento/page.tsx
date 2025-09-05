601fec00b69a31d457d24a63a438ba8efbc0750b
ajamardo <agustin.jch@gmail.com>
2025-09-05 17:07:01 +0200

    feat: integrate water zones into maintenance creation page
    - Added functionality to fetch water zones using UseCaseService.
    - Updated CreateMaintenancePage to accept water zones as props for improved data handling.
    - Removed redundant water zone fetching from the component, streamlining the data flow.

--- apps/webapp/src/app/dashboard/nuevo-registro/mantenimiento/page.tsx
+++ apps/webapp/src/app/dashboard/nuevo-registro/mantenimiento/page.tsx
@@ -1,8 +1,14 @@
+import { UseCaseService } from 'core'
 import type { NextPage } from 'next'
+import { webAppContainer } from '@/src/core/di/webapp.container'
 import { CreateMaintenancePage } from '@/src/features/maintenance/delivery/create-maintenance.page'
+import { GetWaterZonesQry } from '@/src/features/water-zone/application/get-water-zones.qry'
 
 const Page: NextPage = async () => {
-  return <CreateMaintenancePage />
+  const service = webAppContainer.get<UseCaseService>(UseCaseService.ID)
+  const waterZones = await service.execute(GetWaterZonesQry)
+
+  return <CreateMaintenancePage waterZones={waterZones.map((zone) => zone.toDto())} />
 }
 
 export default Page
